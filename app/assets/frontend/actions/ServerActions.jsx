dc64a03b095a70bac408678f69f84db18d4e629c
Araphel <bartekus@gmail.com>
2015-10-27 01:55:50 -0700

    Converted the whole frontend implementation to flux pattern, including stores & actions

--- app/assets/frontend/actions/ServerActions.jsx
+++ app/assets/frontend/actions/ServerActions.jsx
@@ -4,8 +4,14 @@ import ActionTypes from "../constants"
 export default {
   receivedTweets(rawTweets) {
     AppDispatcher.dispatch({
-      actionType: actionType.RECEIVED_TWEETS,
+      actionType: ActionTypes.RECEIVED_TWEETS,
       rawTweets
+    })
+  },
+  receivedOneTweet(rawTweet) {
+    AppDispatcher.dispatch({
+      actionType: ActionTypes.RECEIVED_ONE_TWEET,
+      rawTweet
     })
   }
 }
